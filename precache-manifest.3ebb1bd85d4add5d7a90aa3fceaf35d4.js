d040515aa6dcf8f271f01026173b0fb9020b9509
Keertijanm <81510729+Keertijanm@users.noreply.github.com>
2022-07-14 11:11:09 +0530

    Updates

---
+++ precache-manifest.3ebb1bd85d4add5d7a90aa3fceaf35d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0cfe8b0a067b1accddd5",
+    "url": "/rock-paper-scissorkeerti/static/css/main.d6052595.chunk.css"
+  },
+  {
+    "revision": "0cfe8b0a067b1accddd5",
+    "url": "/rock-paper-scissorkeerti/static/js/main.0cfe8b0a.chunk.js"
+  },
+  {
+    "revision": "e94d872e0cc5161a7576",
+    "url": "/rock-paper-scissorkeerti/static/js/1.e94d872e.chunk.js"
+  },
+  {
+    "revision": "4469aed430a087e993ef",
+    "url": "/rock-paper-scissorkeerti/static/css/2.a385b9eb.chunk.css"
+  },
+  {
+    "revision": "4469aed430a087e993ef",
+    "url": "/rock-paper-scissorkeerti/static/js/2.4469aed4.chunk.js"
+  },
+  {
+    "revision": "9f57794e6922e817b89c",
+    "url": "/rock-paper-scissorkeerti/static/js/runtime~main.9f57794e.js"
+  },
+  {
+    "revision": "20dcb584b529d4c29f9c0c4a93dbd11d",
+    "url": "/rock-paper-scissorkeerti/index.html"
+  }
+];
